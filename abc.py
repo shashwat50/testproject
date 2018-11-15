a92b434ce291fce7572fbf864e390962ae625df4
Shashwat Singh <shashwat.singh@PUSSINGH.local>
2018-11-15 12:50:54 +0530

    Code to expand CIDR

--- abc.py
+++ abc.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
-import ipaddress
-net = ipaddress.ip_network('123.45.67.64/27')
-for a in net:
-	print(a)
+# python cidr.py 192.168.1.1/24
+
+import sys, struct, socket
+
+(ip, cidr) = sys.argv[1].split('/')
+cidr = int(cidr) 
+host_bits = 32 - cidr
+i = struct.unpack('>I', socket.inet_aton(ip))[0] # note the endianness
+start = (i >> host_bits) << host_bits # clear the host bits
+end = start | ((1 << host_bits) - 1)
+
+# excludes the first and last address in the subnet
+for i in range(start, end):
+    print(socket.inet_ntoa(struct.pack('>I',i)))
